ff3694c918c4681551d7f8572a26b68d2f8fd531
Corey Canfield <corey.canfield@gmail.com>
2018-10-22 16:47:12 -0400

    Initial Commit

---
+++ SimpleFTP/FTPConn.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFTP
+{
+    class FTPConnProfile
+    {
+
+        private Uri _connUri;
+        private string _connUser;
+        private string _connPass;
+        private string _connPort;
+        private bool _connKeepAlive;
+        private bool _connBinary;
+        private bool _connPassiveMode;
+
+        public FTPConnProfile (Uri uri, string user, string pass, string port, bool keepalive = false, bool binary = true, bool passivemode = true)
+        {
+            ConnUri             = uri;
+            ConnUser           = user;
+            ConnPass           = pass;
+            ConnPort           = port;
+            ConnKeepAlive      = keepalive;
+            ConnBinary         = binary;
+            ConnPassiveMode    = passivemode;
+        }
+
+        public Uri ConnUri
+        {
+            get { return _connUri; }
+            set { _connUri = value; }
+        }
+
+        public string ConnUser
+        {
+            get { return _connUser; }
+            set { _connUser = value; }
+        }
+
+        public string ConnPass
+        {
+            get { return _connPass; }
+            set { _connPass = value; }
+        }
+
+        public string ConnPort
+        {
+            get { return _connPort; }
+            set { _connPort = value; }
+        }
+
+        public bool ConnKeepAlive
+        {
+            get { return _connKeepAlive; }
+            set { _connKeepAlive = value; }
+        }
+
+        public bool ConnBinary
+        {
+            get { return _connBinary; }
+            set { _connBinary = value;}
+        }
+
+        public bool ConnPassiveMode
+        {
+            get { return _connPassiveMode; }
+            set { _connPassiveMode = value; }
+        }
+    }
+}
---
+++ SimpleFTP/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+
+namespace SimpleFTP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            txt_block.Text = "";
+            txt_block.Text += "Starting connection process...\n";
+            Uri providedUri = ParseUrlForFTP(txt_server.Text);
+            EstablishFTPConnection(providedUri);
+        }
+
+        private void EstablishFTPConnection(Uri uri)
+        {
+            FTPConnProfile connProfile = new FTPConnProfile(uri, txt_user.Text, txt_pass.Text, txt_port.Text);
+            
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            request.Credentials = new NetworkCredential(txt_user.Text, txt_pass.Text);
+            request.KeepAlive = false;
+            request.UseBinary = true;
+            request.UsePassive = true;
+            FtpWebResponse response;
+            String serverRsp = "";
+            StreamReader reader;
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                reader = new StreamReader(responseStream);
+                serverRsp = reader.ReadToEnd();
+                txt_block.Text += "Conncted to: " + uri+ "\n";
+                txt_block.Text += "Directory Listing:\n ----------------- \n";
+                txt_block.Text += serverRsp;
+                reader.Close();
+                response.Close();
+            }
+            catch (WebException ex)
+            {
+                txt_block.Text += ex.Message;
+            }           
+        }
+
+        
+        //Checks FTP Url for ftp://
+        private Uri ParseUrlForFTP(string url)
+        {
+            Console.WriteLine(url);
+            
+            if(url.StartsWith("ftp://"))
+            {
+                Console.WriteLine("Parsed URL: " + url);
+               
+                return new Uri(url);
+            } else
+            {
+                string formattedUrl = "ftp://" + url;
+                Console.WriteLine("Parsed URL: " + formattedUrl);
+                return new Uri(formattedUrl);
+            }         
+        }
+
+        private void Btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            txt_block.Text = "";
+        }
+    }
+}
